e3fec2354cbe6273103551d7d20a6b37443a09ea
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-10-08 19:43:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className='picture'></div>
+      <div className='header'></div>
+      <div className='footer'>
+        <h1></h1>
+        <p></p>
+        <div className='plan'></div>
+        <div className='btn-pay'></div>
+        <div className='btn-cancel'></div>
+      </div>
     </div>
   );
 }
