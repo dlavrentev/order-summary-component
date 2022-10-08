4a477185b2b062e7cba14e9a7e74122ae4583740
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-10-08 19:46:11 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className='header'></div>
-      <div className='footer'>
-        <h1></h1>
-        <p></p>
-        <div className='plan'></div>
-        <div className='btn-pay'></div>
-        <div className='btn-cancel'></div>
+      <div className="header"></div>
+      <div className="footer">
+        <h1>Order Summary</h1>
+        <p>
+          You can now listen to millions of songs, audiobooks, and podcasts on
+          any device anywhere you like!
+        </p>
+        <div className="plan">
+          <div className="logo"></div>
+          <div className="pay">
+            <p>Annual Plan</p>
+            <p>$59.99/year</p>
+          </div>
+          <a href="_blank">Change</a>
+        </div>
+        <div className="btn-pay">Proceed to Payment</div>
+        <div className="btn-cancel">Cancel Order</div>
       </div>
     </div>
   );
